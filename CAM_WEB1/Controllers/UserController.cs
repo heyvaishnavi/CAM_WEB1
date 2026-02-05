d912b6910e11af2051f55c628df81ed996be8a19
Sakthivelan <2467566@cognizant.com>
2026-02-05 17:14:00 +0530

    Changed

--- CAM_WEB1/Controllers/UserController.cs
+++ CAM_WEB1/Controllers/UserController.cs
@@ -146,7 +146,7 @@ namespace CAM_WEB1.Controllers
     public class CreateUserDto
     {
         public string Name { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; }    
         public string Role { get; set; }
         public string? Branch { get; set; }
     }
